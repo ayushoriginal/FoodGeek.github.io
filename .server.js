13504e110fae5f7891253fcbd301a3009598f89e
ayushoriginal <ayush.original@gmail.com>
2016-11-16 16:16:21 +0530

    Initial commit

---
+++ .server.js
@@ -0,0 +1,19 @@
+var api = require('place-my-order-api');
+var server = require('done-serve');
+
+var app = server({
+  path: __dirname,
+  configure: function(app) {
+    app.use('/api', api);
+    app.api = api;
+
+    var oldListen = app.listen;
+    app.listen = function() {
+      var server = oldListen.apply(this, arguments);
+      api.setup(server);
+      return server;
+    };
+  }
+});
+
+app.listen(process.env.PORT || 3030);
